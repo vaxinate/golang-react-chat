c7ac857477bb742ab3695546e1b173ee1768772d
Adam Clarke <adamclarkewastaken@gmail.com>
2024-07-01 15:45:13 -0400

    implement basic GUI client with rx/tx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, ChangeEvent, useReducer, useEffect, useCallback } from "react";
+import useWebSocket, { ReadyState, SendMessage } from "react-use-websocket";
+import { Card, Input, Button, Typography, Textarea} from "@material-tailwind/react";
+
 import './App.css'
 
+interface FormProps {
+  sendMessage: SendMessage,
+};
+
+function Form({ sendMessage }: FormProps) {
+
+  const [message, setMessage] = useState<string>("");
+
+  function onInputChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    return setMessage(e.currentTarget?.value)
+  }
+
+  function onClickSend() {
+    sendMessage(message);
+
+    return setMessage("")
+  }
+  
+  return (
+    <Card color="transparent" shadow={false} className="p-6 w-80">
+      <Typography variant="h4">
+        Send a message.
+      </Typography>
+      <form className="mt-8 mb-2">
+        <div>
+          <Textarea label="Message Text" value={message} onInput={onInputChange}/>
+        </div>
+          <Button className="mt-6" onClick={onClickSend} fullWidth>
+            Send Message
+          </Button>
+      </form>
+    </Card>
+  )
+}
+
+interface ResponseProps {
+  messageText: string | undefined,
+};
+
+function Response({ messageText }: ResponseProps) {
+  if (messageText)
+    return (
+    <Card className="w-80 ml-6 p-3">
+        {messageText}
+      </Card>
+    )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { sendMessage, lastMessage } = useWebSocket("ws://localhost:8080/ws");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form sendMessage={sendMessage}/>
+      <Response messageText={lastMessage?.data} />
     </>
   )
 }
